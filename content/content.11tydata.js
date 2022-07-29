eb927d2da13d157704ac823eaaf26ccd10b2b6d4
Miriam Suzanne <miriam@oddbird.net>
2022-07-29 00:25:21 -0600

    Secondary nav

--- content/content.11tydata.js
+++ content/content.11tydata.js
@@ -1,10 +1,34 @@
 'use strict';
 
 const tags = require('../src/filters/tags');
+const { taxonomy } = require('../src/filters/taxonomy');
 
-const link = (text, url, category = true) => {
+const linkHtml = (text, url, category = true) => {
   const attrs = category ? `class="p-category"` : '';
   return `<a href="${url}" ${attrs}>${text}</a>`;
+};
+
+let memo = null;
+
+const tagLinks = async (data) => {
+  if (memo) {
+    return memo;
+  }
+
+  const linked = await taxonomy.tags.map((name) => {
+    const url = tags.tagLink(name, data.collections);
+    const link = linkHtml(name, url);
+    return { name, url, link };
+  });
+
+  linked.push({
+    name: 'more',
+    url: '/tags/',
+    link: linkHtml('more', '/tags/', false),
+  });
+
+  memo = [...linked];
+  return linked;
 };
 
 const replaceTags = async (source, data, text = false) => {
@@ -17,23 +41,13 @@ const replaceTags = async (source, data, text = false) => {
     return source;
   }
 
-  const topTags = await tags.getTags(data.collections, true);
-
-  const tagsHTML = topTags
-    .map((tag) => {
-      const name = tags.displayName(tag);
-      const url = tags.tagLink(tag, data.collections);
-      return text ? name : link(name, url);
-    })
-    .join(' & ');
-
-  const more = text ? 'more' : link('more', '/tags/', false);
-
-  return `${tagsHTML} & ${more}`;
+  const links = await tagLinks(data);
+  return links.map((tag) => (text ? tag.name : tag.link)).join(' & ');
 };
 
 module.exports = {
   eleventyComputed: {
+    top_tags: (data) => tagLinks(data),
     banner: (data) => replaceTags(data.banner || data.title, data),
     sub: (data) => replaceTags(data.sub, data),
     description: (data) =>
