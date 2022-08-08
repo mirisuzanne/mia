bf237dec6f9beb12fc3b5380d4a13a962f0b4c4c
Miriam Suzanne <miriam@oddbird.net>
2022-08-08 10:53:10 -0600

    Use media attr for controlling styles

--- src/js/css.js
+++ src/js/css.js
@@ -3,15 +3,49 @@ export default function() {
   const cssLinks = document.querySelectorAll(`link[rel*=style]`);
   const cssBtns = document.querySelectorAll(`[data-options=css] [data-set]`);
 
-  const getStyle = () => localStorage.getItem('css') || root.dataset.css;
+  let btnTargets = [];
+
+  cssBtns.forEach((btn) => {
+    const set = btn.dataset.set;
+    if (set && !btnTargets.includes(set)) {
+      btnTargets = [...btnTargets, set];
+    }
+  });
+
+  let layers = [];
+  let alts = [];
+
+  cssLinks.forEach((link) => {
+    const layer = link.dataset.layer;
+    const alt = link.dataset.alt;
+
+    if (layer && !layers.includes(layer)) {
+      layers = [...layers, layer];
+    } else if (alt && !alts.includes(alt)) {
+      alts = [...alts, alt];
+    }
+  });
+
+  const getStyle = () => {
+    const current = localStorage.getItem('css') || root.dataset.css;
+    return btnTargets.includes(current) ? current : layers.at(-1);
+  };
 
   const updateStyle = (to) => {
-    const set = typeof to === 'undefined' ? getStyle() : to;
+    const set = to || getStyle();
 
-    if (set) {
+    if (layers.includes(set)) {
+      const setIndex = layers.indexOf(set);
       cssLinks.forEach((link) => {
-        const isActive = link.getAttribute('title') === set;
-        link.disabled = !isActive;
+        const isActive = link.dataset.layer
+          ? layers.indexOf(link.dataset.layer) <= setIndex
+          : false;
+        link.setAttribute('media', isActive ? 'all' : 'none');
+      });
+    } else {
+      cssLinks.forEach((link) => {
+        const isActive = link.dataset.alt === set;
+        link.setAttribute('media', isActive ? 'all' : 'none');
       });
     }
   };
